945eb158ee143bd61a918dfd1addc77362d8b441
railey1990 <marcel.teiting@t-online.de>
2017-11-18 16:42:45 +0100

    Added IsNullOrEmpty extension method for convenience

--- Box.Core/Extensions/StringExtensions.cs
+++ Box.Core/Extensions/StringExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class StringExtensions
     {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+        
         public static string FirstCharToLower( this string @string )
         {
             return char.ToLower( @string[0] ) + @string.Substring( 1, @string.Length - 1 );
